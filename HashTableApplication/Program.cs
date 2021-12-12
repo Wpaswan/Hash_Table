48dba47ae732a15ca297066ae4ecde102aa51dc3
Wpaswan <dablumcapaswan@gmail.com>
2021-12-11 22:10:52 -0800

    [I,added a code to to count the frequency of word]

---
+++ HashTableApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using HashTableApplication;
+Console.WriteLine("Hello World!WELCOME TO HASHTABLE");
+Console.WriteLine("Hash table demo"); //() []
+MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+hash.Add("0", "To be or not to be");
+hash.Add("0", "To be or not to be");
+hash.frequencyOfWords("0");
+hash.Add("1", "be");
+hash.Add("2", "not");
+hash.Add("3", "or");
+hash.Add("4", "not");
+hash.Add("5", "be");
+string hash5 = hash.Get("5");
+Console.WriteLine("Fifth index value="+hash5);
+
+string hash2 = hash.Get("2");
+Console.WriteLine("Fifth index value="+hash2);
